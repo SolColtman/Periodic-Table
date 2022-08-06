4f71d7c1bc4524d700666d471b88590799fb462f
psysc9 <psysc9@exmail.nottingham.ac.uk>
2022-08-06 14:49:45 +0100

    made getCSVField function

--- main.c
+++ main.c
@@ -2,8 +2,53 @@
 // Created by SolColtman on 06/08/2022.
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("Hello, World!");
+struct element{
+    char name[16];
+    char symbol[2];
+    int number;
+    float mass;
+};
+
+struct element elements[118];
+char* getCSVField(char *line, int num);
+
+int main(int argc, char **argv){
+    FILE *fp;
+    char line[256];
+    char *item;
+
+    if (!(fp=fopen("index.csv", "r"))){
+        fprintf(stderr, "errorL unable to read file %s\n", "index.csv");
+        exit(EXIT_FAILURE);
+    }
+
+    int check=fgetc(fp);
+    if(check==EOF)
+        exit(EXIT_FAILURE);
+
+    while(fgets(line, 256, fp)){
+        char* temp = strdup(line);
+        item=getCSVField(temp, 4);
+        printf("%s\n", item);
+        free(temp);
+    }
+
     return 0;
+}
+
+char* getCSVField(char *line, int num){
+
+    char* token;
+    for (token = strtok(line, ",");
+            token && *token;
+            token = strtok(NULL, ",/n"))
+    {
+        if (!--num)
+            return token;
+    }
+
+    return NULL;
 }
